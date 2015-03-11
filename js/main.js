c625ded01f8fe550776c16dcdaf7b9744a80b927
Gabriel Farrell <g@gsf.io>
2015-03-11 00:21:14 -0400

    Render in main

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+function render () {
+  // Check route and display corresponding view
+}
+
+// Initialize
 $.ajax('templates.html').done(function (html) {
   app.templates = $(html);
-  views();
+  render();
 });
+
+// Handle traversing of history
+window.onpopstate = render;
