f62e01111de53052582cda52433a4c188e00d651
Gabriel Farrell <g@gsf.io>
2015-03-12 17:40:02 -0400

    Build working. Generate bundle. Update index.

--- build.js
+++ build.js
@@ -1,8 +1,15 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
+var minify = require('uglify-js').minify;
 
 $ = cheerio.load(fs.readFileSync('dev.html'));
 
+var files = [];
+
 $('script[src^=js]').each(function (i, el) {
-  console.log(el.attribs.src);
+  files.push($(el).attr('src'));
 });
+
+console.log(files);
+
+fs.writeFileSync('bundle.js', minify(files).code);
