b6ef31322fc1b7f7a7e36db0a439cf88f8d96301
Gabriel Farrell <g@gsf.io>
2015-03-12 12:17:42 -0400

    Search results like patterns. Fix #5

--- js/views.js
+++ js/views.js
@@ -4,27 +4,29 @@ app.views = {
   },
   count: function (total) {
     app.els.count.find('#total').text(total);
-    app.els.count.appendTo(app.els.results);
+    app.els.count.appendTo(app.els.content);
   },
   result: function (address) {
     // Clone and append to #results
     var result = app.els.result.clone();
     console.log(address);
-    result.find('h3').text(address.standardizedAddress);
+    result.removeAttr('id');
+    var key = address.standardizedAddress;
+    result.find('a').attr('href', '?' + $.param({a: key})).text(key);
     result.appendTo(app.els.results);
   },
   results: function (q) {
-    app.els.results.appendTo(app.els.content);
-    app.els.results.text('Loading...');
-
+    app.els.content.text('Loading...');
     $.ajax('https://api.phila.gov/ulrs/v3/addresses/' + q + '?format=json')
       .done(function (data) {
-        app.els.results.empty();
+        app.els.content.empty();
         app.views.count(data.addresses.length);
+        app.els.results.empty();
         data.addresses.forEach(app.views.result);
+        app.els.results.appendTo(app.els.content);
       })
       .fail(function (jqXHR, status, err) {
-        app.els.results.text('Failed to retrieve results. Please try another search.');
+        app.els.content.text('Failed to retrieve results. Please try another search.');
       });
   }
 }
