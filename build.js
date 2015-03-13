a62adbaafc0948e547269abcba1ae943b84ef93d
Gabriel Farrell <g@gsf.io>
2015-03-13 11:58:57 -0400

    Better console output from build.js

--- build.js
+++ build.js
@@ -4,12 +4,14 @@ var minify = require('uglify-js').minify;
 
 $ = cheerio.load(fs.readFileSync('dev.html'));
 
+process.stdout.write('JS files sourced in dev.html... ');
 var files = [];
-
 $('script[src^=js]').each(function (i, el) {
-  files.push($(el).attr('src'));
+  var file = $(el).attr('src');
+  process.stdout.write(file + ' ');
+  files.push(file);
 });
 
-console.log(files);
-
+process.stdout.write('\nConcatenating and minifying... ');
 fs.writeFileSync('bundle.js', minify(files).code);
+process.stdout.write('bundle.js\n');
