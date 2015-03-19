576fe3b16bba11d59477064a129bb7477f5ca4f3
Gabriel Farrell <g@gsf.io>
2015-03-18 21:20:53 -0400

    Call pushState for relative links. Fix #12

--- js/events.js
+++ js/events.js
@@ -8,3 +8,14 @@ app.els.search.parent().on('submit', function (e) {
   history.pushState(null, q.value, '?' + $.param({q: q.value}));
   app.render(e);
 });
+
+$(document).on('click', 'a', function (e) {
+  if (!history.pushState) return;
+  if (e.defaultPrevented) return;
+  var a = $(e.target)
+  var href = a.attr('href');
+  if (href.indexOf('?') && href !== window.location.pathname) return;
+  e.preventDefault();
+  history.pushState(null, a.text(), href);
+  app.render(e);
+});
