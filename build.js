5a265c3dd58c17a03a804be4a3fa7efd932fdc77
Gabriel Farrell <g@gsf.io>
2015-03-25 15:50:03 -0400

    Generate index.html with build. Fix #16

--- build.js
+++ build.js
@@ -2,16 +2,29 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var minify = require('uglify-js').minify;
 
-$ = cheerio.load(fs.readFileSync('dev.html'));
+var devFile = 'dev.html';
+var jsFile = 'bundle.js';
+var prodFile = 'index.html';
 
-process.stdout.write('JS files sourced in dev.html... ');
+process.stdout.write('Loading dev file... ');
+var $ = cheerio.load(fs.readFileSync(devFile), {decodeEntities: false});
+process.stdout.write(devFile);
+
+process.stdout.write('\nCollecting JS files sourced in dev file...');
 var files = [];
 $('script[src^=js]').each(function (i, el) {
-  var file = $(el).attr('src');
-  process.stdout.write(file + ' ');
+  var $el = $(el);
+  var file = $el.attr('src');
+  process.stdout.write(' ' + file);
   files.push(file);
+  if (i === 0) $el.attr('src', jsFile);
+  else $el.remove();
 });
 
-process.stdout.write('\nConcatenating and minifying... ');
-fs.writeFileSync('bundle.js', minify(files).code);
-process.stdout.write('bundle.js\n');
+process.stdout.write('\nConcatenating and minifying JS... ');
+fs.writeFileSync(jsFile, minify(files).code);
+process.stdout.write(jsFile);
+
+process.stdout.write('\nCopying dev file with replaced script tags... ');
+fs.writeFileSync(prodFile, $.html().replace(/^ *\n/gm, ''), 'utf8');
+process.stdout.write(prodFile + '\n');
