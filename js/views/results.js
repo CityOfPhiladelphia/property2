4665593b19dfb96afd147df6a4953cea1a097f68
Gabriel Farrell <g@gsf.io>
2015-04-14 17:30:08 -0400

    Button to view more results. Fix #15

--- js/views/results.js
+++ js/views/results.js
@@ -17,18 +17,18 @@ app.views.results = function (q) {
   app.hooks.content.children().detach();
   app.hooks.belowContent.children().detach();
 
+  // Determine if q is an account number, intersection, or address
+  q = q.trim();
+  var m, opaEndpoint;
+  if (m = /#?(\d{9})/.exec(q)) opaEndpoint = 'account/' + m[1];
+  else if (m = /(.+) +(&|and) +(.+)/.exec(q)) {
+    opaEndpoint = 'intersection/' + encodeURI(m[1] + '/' + m[3])
+  } else opaEndpoint = 'address/' + encodeURIComponent(opaAddress(q));
+
   if (history.state) {
     render();
   } else {
     app.hooks.content.append(app.hooks.loading);
-    var q = q.trim();
-
-    // Determine if q is an account number, intersection, or address
-    var m, opaEndpoint;
-    if (m = /#?(\d{9})/.exec(q)) opaEndpoint = 'account/' + m[1];
-    else if (m = /(.+) +(&|and) +(.+)/.exec(q)) {
-      opaEndpoint = 'intersection/' + encodeURI(m[1] + '/' + m[3])
-    } else opaEndpoint = 'address/' + encodeURIComponent(opaAddress(q));
 
     $.ajax('https://api.phila.gov/opa/v1.1/' + opaEndpoint + '?format=json')
       .done(function (data) {
@@ -66,28 +66,45 @@ app.views.results = function (q) {
     //app.hooks.count.find('#total').text(state.total);
     //app.hooks.content.append(app.hooks.count);
     app.hooks.resultRows.empty(); // TODO reuse existing result nodes
-    state.data.properties.forEach(function (property) {
-      var row = app.hooks.resultRow.clone();
-      var accountNumber = property.account_number;
-      var withUnit = app.util.addressWithUnit(property);
-      var href = '?' + $.param({p: accountNumber});
-      row.append($('<td>').append($('<a href="' + href + '">').text(withUnit)));
-      row.append($('<td>').text(accounting.formatMoney(property.valuation_history[0].market_value)));
-      row.append($('<td class="hide-for-small">').text(app.util.formatSalesDate(property.sales_information.sales_date)
-        + ', ' + accounting.formatMoney(property.sales_information.sales_price)));
-      row.append($('<td class="hide-for-small">').text(property.ownership.owners.join(', ')));
-      row.append($('<td>').html('<i class="fa fa-arrow-circle-right"></i>'));
-      row.on('click', function (e) {
-          if (e.ctrlKey || e.altKey || e.shiftKey) return;
-          e.preventDefault();
-          history.pushState({opa: property, address: withUnit}, withUnit, href);
-          window.scroll(0, 0);
-          app.views.property(accountNumber);
-        });
-      app.hooks.resultRows.append(row);
-    });
-  //  app.hooks.belowContent.empty();
+    state.data.properties.forEach(addRow);
+    if (state.total > state.data.properties.length) {
+      var seeMoreA = app.hooks.seeMore.find('a');
+      seeMoreA.off('click'); // Drop previously created click events
+      seeMoreA.on('click', function (e) {
+        $.ajax('https://api.phila.gov/opa/v1.1/' + opaEndpoint + '?format=json&skip=' + state.data.properties.length)
+          .done(function (data) {
+            state.data.properties = state.data.properties.concat(data.data.properties);
+            history.replaceState(state, ''); // Second param not optional in IE10
+            data.data.properties.forEach(addRow);
+            if (state.total === state.data.properties.length) app.hooks.seeMore.hide();
+          })
+      });
+      app.hooks.seeMore.show();
+    } else {
+      app.hooks.seeMore.hide();
+    }
     app.hooks.content.append(app.hooks.results);
+  }
+
+  function addRow (property) {
+    var row = app.hooks.resultRow.clone();
+    var accountNumber = property.account_number;
+    var withUnit = app.util.addressWithUnit(property);
+    var href = '?' + $.param({p: accountNumber});
+    row.append($('<td>').append($('<a href="' + href + '">').text(withUnit)));
+    row.append($('<td>').text(accounting.formatMoney(property.valuation_history[0].market_value)));
+    row.append($('<td class="hide-for-small">').text(app.util.formatSalesDate(property.sales_information.sales_date)
+      + ', ' + accounting.formatMoney(property.sales_information.sales_price)));
+    row.append($('<td class="hide-for-small">').text(property.ownership.owners.join(', ')));
+    row.append($('<td>').html('<i class="fa fa-arrow-circle-right"></i>'));
+    row.on('click', function (e) {
+        if (e.ctrlKey || e.altKey || e.shiftKey) return;
+        e.preventDefault();
+        history.pushState({opa: property, address: withUnit}, withUnit, href);
+        window.scroll(0, 0);
+        app.views.property(accountNumber);
+      });
+    app.hooks.resultRows.append(row);
   }
 
   // OPA address needs to either separate unit by slash or end in slash
