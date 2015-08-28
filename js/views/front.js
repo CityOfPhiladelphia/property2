b197382cec6e433306438de18ae4f4053ae1140a
Aaron Ogle <atogle@gmail.com>
2015-08-28 11:16:37 -0400

    Add owner to the search field placeholder text.

--- js/views/front.js
+++ js/views/front.js
@@ -3,9 +3,9 @@
 app.views.front = function () {
   app.hooks.crumbs.update();
 
-  app.hooks.search.val('').attr('placeholder', 'Enter address, address range, account number, or intersection');
+  app.hooks.search.val('').attr('placeholder', 'Enter address, address range, owner, account, or intersection');
   if($( window ).width() <= '480'){
-    app.hooks.search.val('').attr('placeholder','Enter address, account number, etc.');
+    app.hooks.search.val('').attr('placeholder','Enter address, owner, account, etc.');
   }
   app.hooks.searchRight.html('&nbsp;');
   app.hooks.searchLeft.removeClass('medium-14').addClass('medium-4').html('&nbsp;');
