7e6bc2a8dd72242b6a3239830d9df68f3c0e5e29
Robert Martin <rmartin@rmart.in>
2017-02-09 11:40:06 -0500

    Remove String.repeat polyfill; no longer needed

--- js/app.js
+++ js/app.js
@@ -337,53 +337,8 @@ app.util.constructTencode = function (aisObj) {
   // check for unit num and pad
   if (unitNum.length > 0) {
     var unitNumPadded = '0'.repeat(7 - unitNum.length) + unitNum;
-    tencode += unitNumPadded; 
+    tencode += unitNumPadded;
   }
 
   return tencode;
-}
-
-// polyfill for String.repeat() on browsers without ES6 support
-if (!String.prototype.repeat) {
-  String.prototype.repeat = function(count) {
-    'use strict';
-    if (this == null) {
-      throw new TypeError('can\'t convert ' + this + ' to object');
-    }
-    var str = '' + this;
-    count = +count;
-    if (count != count) {
-      count = 0;
-    }
-    if (count < 0) {
-      throw new RangeError('repeat count must be non-negative');
-    }
-    if (count == Infinity) {
-      throw new RangeError('repeat count must be less than infinity');
-    }
-    count = Math.floor(count);
-    if (str.length == 0 || count == 0) {
-      return '';
-    }
-    // Ensuring count is a 31-bit integer allows us to heavily optimize the
-    // main part. But anyway, most current (August 2014) browsers can't handle
-    // strings 1 << 28 chars or longer, so:
-    if (str.length * count >= 1 << 28) {
-      throw new RangeError('repeat count must not overflow maximum string size');
-    }
-    var rpt = '';
-    for (;;) {
-      if ((count & 1) == 1) {
-        rpt += str;
-      }
-      count >>>= 1;
-      if (count == 0) {
-        break;
-      }
-      str += str;
-    }
-    // Could we try:
-    // return Array(count + 1).join(this);
-    return rpt;
-  }
 }
